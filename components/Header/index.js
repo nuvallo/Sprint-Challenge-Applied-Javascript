a00dcc6c2753583e6c12ea074c9d55ecfa229dd8
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2019-11-08 21:08:16 -0500

    Elements created for header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,11 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector(".header-container");
+function Header() {
+  // Elements
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const heading = document.createElement("h1");
+  const temp = document.createElement("span");
+}
