a828eb6a4a3f5356c79fb9b700e8a8afeb778a62
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2019-11-08 21:10:28 -0500

    Classes added to elements and text content added in for header

--- components/Header/index.js
+++ components/Header/index.js
@@ -16,4 +16,14 @@ function Header() {
   const date = document.createElement("span");
   const heading = document.createElement("h1");
   const temp = document.createElement("span");
+
+  // classes
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+
+  // Content
+  date.textContent = "SMARCH 28, 2019";
+  heading.textContent = "Lambda Times";
+  temp.textContent = "98";
 }
